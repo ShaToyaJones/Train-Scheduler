b7bec43c1b085d8b8221a8d1909a3a1f10516363
ShaToyaJones <jshatoyaj@yahoo.com>
2017-09-21 22:45:33 -0400

    major changes

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,41 +1,88 @@
-//initialize firebase
-var config = {
-	apiKey: "AIzaSyAqPKB4tnPYxMSph9_3rti-CnHX94E_Rcw",
-    authDomain: "train-scheduler-b25b8.firebaseapp.com",
-    databaseURL: "https://train-scheduler-b25b8.firebaseio.com",
-    projectId: "train-scheduler-b25b8",
-    storageBucket: "train-scheduler-b25b8.appspot.com",
-    messagingSenderId: "85601842854"
-  };
+//
+var db = firebase.database();
 
-  firebase.initializeApp(config);
-  
-};
+function addTrain(e) {
+	//Stops the webpage from refreshing.
+	e.preventDefault();
 
-$(document).ready(function(){
+	//variables for train schedule
+	//jQuery grabs user input, assigns to variable.
+	var tName = $("#trainName").val().trim();
+	var destination = $("#destination").val().trim();
+	var frequency = $("#frequency").val().trim();
 
-$("#addInfo").on("click", function() {
+	//0, null, false, undefined, Nan, ""
+	if (!tName || !destination || !frequency) return;
 
-//variables for train schedule
-//jQuery grabs user input, assigns to variable.
-var database = firebase.database();
-var tName = $("#trainName").val().trim();
-var destination = $("#destination").val().trim();
-var arrivalTime = $("#arrivalTime").val().trim();
-var departureTime = $("#departureTime").val().trim();
-var Frequency = $("#frequency").val().trim();
-var minutesAway = $("#minutesAway").val().trim();
-var nextArrival = $("#nextArrival").val().trim();
+	db.ref("trains").push({
+		name: tName,
+		destination: destination,
+		frequency: frequency
+	});
 
-console.log(trainName);
-console.log(destination);
-console.log(arrivalTime);
-console.log(departureTime);
-console.log(Frequency);
-console.log(minutesAway);
-console.log(nextArrival);
+}
 
-}};
+function getTrains(){
+	//Outputting the next arrival time.
+	console.log(moment().add(20, "minutes").format("HH:mm"));
+	db.ref("trains").on("child_added", function (train) {
+		train = train.val();
 
-//new variables for storage of new input
+		$("#tableTrain tbody").append(
+			"<tr>" +
+				"<td>" + train.name + "</td>" +
+				"<td>" + train.destination + "</td>" +
+				"<td>" + train.frequency + "</td>" +
+				"<td>Coming Soon</td>" +
+				"<td>Coming Soon</td>" +
+			"</tr>"
+		);
 
+	});
+}
+
+getTrains();
+$("#addInfo").on("click", addTrain);
+
+//The train frequency.
+var tFrequency = 15;
+
+//The first time a train leaves for the day.
+var firstTime = "05:00";
+
+//The first time is pushed back 1 year to make sure it comes before current time.
+var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1, "years");
+	console.log(firstTimeConverted);
+
+//Display current time.
+var currentTime = moment();
+	console.log("Current Time: " + moment(currentTime).format("hh:mm"));
+
+//The difference between the times.
+var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+	console.log("Difference in Time: " + diffTime);
+
+//The time apart/the remainder.
+var tRemainder = diffTime % tFrequency;
+	console.log(tRemainder);
+
+//Minutes until train.
+var tMinutesUntilTrain = tFrequency - tRemainder;
+	console.log("Minutes until next train: " + tMinutesUntilTrain);
+
+//Minutes until the next train.
+var nextTrain = moment().add(tMinutesUntilTrain, "minutes");
+	console.log("Arrival Time: " + moment(nextTrain).format("hh:mm"));
+
+//formula that converts arrival time + current time into minutes
+// var arrivalcurrentMinutes = moment()
+
+//gets the length of the duration in minutes
+// moment.duration().asMinutes()
+//get the difference between the two
+
+//divide the amount of trains that have left since the last time
+
+//get the remainder
+
+//number of minutes - the amount of trains that have left
